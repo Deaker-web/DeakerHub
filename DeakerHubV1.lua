2194eb2fb9561b3af1ab8081f1b6d4b922fa476c
Deaker-web <makaradar893@gmail.com>
2025-04-30 08:07:43 +0300

    Update DeakerHubV1.lua

--- DeakerHubV1.lua
+++ DeakerHubV1.lua
@@ -1,74 +1,489 @@
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+if not _G.FullBrightExecuted then
 
-local Window = Rayfield:CreateWindow({
-   Name = "Deaker Hub",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Deaker Interface Suite",
-   LoadingSubtitle = "by profeh",
-   Theme = "Amethyst", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+    _G.FullBrightEnabled = false
 
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+    _G.NormalLightingSettings = {
+        Brightness = game:GetService("Lighting").Brightness,
+        ClockTime = game:GetService("Lighting").ClockTime,
+        FogEnd = game:GetService("Lighting").FogEnd,
+        GlobalShadows = game:GetService("Lighting").GlobalShadows,
+        Ambient = game:GetService("Lighting").Ambient
+    }
 
-   ConfigurationSaving = {
-      Enabled = false,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Deaker Hub"
-   },
+    game:GetService("Lighting"):GetPropertyChangedSignal("Brightness"):Connect(function()
+        if game:GetService("Lighting").Brightness ~= 1 and game:GetService("Lighting").Brightness ~= _G.NormalLightingSettings.Brightness then
+            _G.NormalLightingSettings.Brightness = game:GetService("Lighting").Brightness
+            if not _G.FullBrightEnabled then
+                repeat
+                    wait()
+                until _G.FullBrightEnabled
+            end
+            game:GetService("Lighting").Brightness = 1
+        end
+    end)
 
-   Discord = {
-      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "discord.gg/https://EUSSWgT5Gg", -- The Discord invite code, do not include . E.g. discord.gg/https://EUSSWgT5Gg ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
+    game:GetService("Lighting"):GetPropertyChangedSignal("ClockTime"):Connect(function()
+        if game:GetService("Lighting").ClockTime ~= 12 and game:GetService("Lighting").ClockTime ~= _G.NormalLightingSettings.ClockTime then
+            _G.NormalLightingSettings.ClockTime = game:GetService("Lighting").ClockTime
+            if not _G.FullBrightEnabled then
+                repeat
+                    wait()
+                until _G.FullBrightEnabled
+            end
+            game:GetService("Lighting").ClockTime = 12
+        end
+    end)
 
-   KeySystem = true, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Key System | @profeh_pot",
-      Subtitle = "Deaker Hub",
-      Note = "Join discord | https://discord.gg/EUSSWgT5Gg", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"deaker.wtf"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
-})
+    game:GetService("Lighting"):GetPropertyChangedSignal("FogEnd"):Connect(function()
+        if game:GetService("Lighting").FogEnd ~= 786543 and game:GetService("Lighting").FogEnd ~= _G.NormalLightingSettings.FogEnd then
+            _G.NormalLightingSettings.FogEnd = game:GetService("Lighting").FogEnd
+            if not _G.FullBrightEnabled then
+                repeat
+                    wait()
+                until _G.FullBrightEnabled
+            end
+            game:GetService("Lighting").FogEnd = 786543
+        end
+    end)
 
-Rayfield:Notify({
-   Title = "follow to discord",
-   Content = "https://discord.gg/EUSSWgT5Gg",
-   Duration = 3.5,
-   Image = 4483362458,
-})
+    game:GetService("Lighting"):GetPropertyChangedSignal("GlobalShadows"):Connect(function()
+        if game:GetService("Lighting").GlobalShadows ~= false and game:GetService("Lighting").GlobalShadows ~= _G.NormalLightingSettings.GlobalShadows then
+            _G.NormalLightingSettings.GlobalShadows = game:GetService("Lighting").GlobalShadows
+            if not _G.FullBrightEnabled then
+                repeat
+                    wait()
+                until _G.FullBrightEnabled
+            end
+            game:GetService("Lighting").GlobalShadows = false
+        end
+    end)
 
-local PlayerTab = Window:CreateTab("Main", 4483362458) -- Title, Image
-local Divider = PlayerTab:CreateDivider()
+    game:GetService("Lighting"):GetPropertyChangedSignal("Ambient"):Connect(function()
+        if game:GetService("Lighting").Ambient ~= Color3.fromRGB(178, 178, 178) and game:GetService("Lighting").Ambient ~= _G.NormalLightingSettings.Ambient then
+            _G.NormalLightingSettings.Ambient = game:GetService("Lighting").Ambient
+            if not _G.FullBrightEnabled then
+                repeat
+                    wait()
+                until _G.FullBrightEnabled
+            end
+            game:GetService("Lighting").Ambient = Color3.fromRGB(178, 178, 178)
+        end
+    end)
 
-local Section = PlayerTab:CreateSection("Player")
+    game:GetService("Lighting").Brightness = 1
+    game:GetService("Lighting").ClockTime = 12
+    game:GetService("Lighting").FogEnd = 786543
+    game:GetService("Lighting").GlobalShadows = false
+    game:GetService("Lighting").Ambient = Color3.fromRGB(178, 178, 178)
 
-Section:Set("Player")
+    local LatestValue = true
+    spawn(function()
+        repeat
+            wait()
+        until _G.FullBrightEnabled
+        while wait() do
+            if _G.FullBrightEnabled ~= LatestValue then
+                if not _G.FullBrightEnabled then
+                    game:GetService("Lighting").Brightness = _G.NormalLightingSettings.Brightness
+                    game:GetService("Lighting").ClockTime = _G.NormalLightingSettings.ClockTime
+                    game:GetService("Lighting").FogEnd = _G.NormalLightingSettings.FogEnd
+                    game:GetService("Lighting").GlobalShadows = _G.NormalLightingSettings.GlobalShadows
+                    game:GetService("Lighting").Ambient = _G.NormalLightingSettings.Ambient
+                else
+                    game:GetService("Lighting").Brightness = 1
+                    game:GetService("Lighting").ClockTime = 12
+                    game:GetService("Lighting").FogEnd = 786543
+                    game:GetService("Lighting").GlobalShadows = false
+                    game:GetService("Lighting").Ambient = Color3.fromRGB(178, 178, 178)
+                end
+                LatestValue = not LatestValue
+            end
+        end
+    end)
+end
 
-local Slider = PlayerTab:CreateSlider({
-    Name = "Walk Speed",
-    Range = {10, 100},
-    Increment = 10,
-    Suffix = "WalkSpeed",
-    CurrentValue = 10,
-    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-    Callback = function(Value)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
-    end,
- })
+_G.FullBrightExecuted = false
+_G.FullBrightEnabled = false
 
- local Slider = PlayerTab:CreateSlider({
-    Name = "Health",
-    Range = {100, 9999},
-    Increment = 10,
-    Suffix = "Health",
-    CurrentValue = 10,
-    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-    Callback = function(Value)
-    game.Players.LocalPlayer.Character.Humanoid.Health = Value
-    game.Players.LocalPlayer.Character.Humanoid.Health = Value
-    end,
- })
+
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Consistt/Ui/main/UnLeaked"))()
+
+
+library.rank = "developer"
+local Wm = library:Watermark("Deaker.Hub | v" .. library.version ..  " | " .. library:GetUsername() .. " | rank: " .. library.rank)
+local FpsWm = Wm:AddWatermark("fps: " .. library.fps)
+coroutine.wrap(function()
+    while wait(.75) do
+        FpsWm:Text("fps: " .. library.fps)
+    end
+end)()
+
+
+local Notif = library:InitNotifications()
+
+for i = 20,0,-1 do 
+    task.wait(0.05)
+    local LoadingXSX = Notif:Notify("Loading DeakerHub V1", 3, "information") -- notification, alert, error, success, information
+end 
+
+library.title = "DeakerHub"
+
+library:Introduction()
+wait(1)
+local Init = library:Init()
+
+local Tab1 = Init:NewTab("Main")
+
+local Section1 = Tab1:NewSection("Main Functions")
+
+
+local Label1 = Tab1:NewLabel("Esp and Aim", "left")--"left", "center", "right"
+
+local Button1 = Tab1:NewButton("ESP Box", function()
+    local ESPLoad = loadstring(game:HttpGet("https://raw.githubusercontent.com/Deaker-web/DeakerHub/refs/heads/main/Aim.lua"))()
+ESPLoad.Load()
+end)
+
+
+local Button1 = Tab1:NewButton("AIM-Bot", function()
+    local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/Deaker-web/DeakerHub/refs/heads/main/aimv2.lua"))()
+Aimbot.Load()
+end)
+
+local Button1 = Tab1:NewButton("FullBright", function()
+    _G.FullBrightEnabled = true
+end)
+
+local Tab2 = Init:NewTab("Player")
+
+local Section2 = Tab2:NewSection("Raje Functions")
+
+local Button1 = Tab1:NewButton("Иди нахуй", function()
+end)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+local FinishedLoading = Notif:Notify("Loaded hub example", 4, "success")
+
+-- // FUNCTION DOCS: 
+--[[
+    MAIN COMPONENT DOCS:
+
+    -- // local library = loadstring(game:HttpGet(link: url))()
+    -- // library.title = text: string
+    -- // local Window = library:Init()
+
+    -- [library.title contains rich text]
+
+    -- / library:Remove()
+    -- destroys the library
+
+    -- / library:Text("new")
+    -- sets the lbrary's text to something new
+
+    - / library:UpdateKeybind(Enum.KeyCode.RightAlt)
+    -- sets the lbrary's keybind to switch visibility to something new
+
+    __________________________
+
+    -- // local notificationLibrary = library:InitNotifications()
+    -- // local Notification = notificationLibrary:Notify(text: string, time: number, type: string (information, notification, alert, error, success))
+
+    -- [Notify contains rich text]
+
+    -- / 3rd argument is a function, used like this:
+    
+    Notif:Notify("Function notification", 7, function()
+        print("done")
+    end)
+
+    -- / Welcome:Text("new text")
+    -- sets the notifications text to something differet [ADDS A +0.4 ONTO YOUR TIMER]
+
+    __________________________
+
+    -- // local Wm = library:Watermark(text: string)
+
+    -- [Watermark contains rich text]
+
+    -- / Wm:Hide()
+    -- hides the watermark from eye view
+
+    -- / Wm:Show()
+    -- makes the watermark visible at the top of your screen
+
+    -- / Wm:Text("new")
+    -- sets the watermark's text to something new
+
+    -- / Wm:Remove()
+    -- destroys the watermark
+
+    __________________________
+
+    -- // local Tab1 = Init:NewTab(text: string)
+
+    -- [tab title contains rich text]
+
+    -- / Tab1:Open()
+    -- opens the tab you want
+
+    -- / Tab1:Remove()
+    -- destroys the tab
+
+    -- / Tab1:Hide()
+    -- hides the tab from eye view
+
+    -- / Tab1:Show()
+    -- makes the tab visible on the selection table
+
+    -- / Tab1:Text("new")
+    -- sets the tab's text to something new
+
+    __________________________
+
+    -- [label contains rich text]
+
+    -- / Label1:Text("new")
+    -- sets the label's text to something new
+
+    -- / Label1:Remove()
+    -- destroys the label
+
+    -- / Label1:Hide()
+    -- hides the label from eye view
+
+    -- / Label1:Show()
+    -- makes the tab visible on the page that is used
+
+    -- / Label1:Align("le")
+    -- aligns the label to a new point in text X
+
+    __________________________
+
+    -- [Button contains rich text]
+
+    -- / Button1:AddButton("text", function() end)
+    -- adds a new button inside of the frame [CAN ONLY GO UP TO 4 BUTTONS AT A TIME]
+
+    -- / Button1:Fire()
+    -- executes the script within the button
+
+    -- / Button1:Text("new")
+    -- sets the button's text to something new
+
+    -- / Button1:Hide()
+    -- hides the button from eye view
+
+    -- / Button1:Show()
+    -- makes the button visible
+
+    -- / Button1:Remove()
+    -- destroys the button
+
+    __________________________
+
+    -- [Sections contain rich text]
+
+    -- / Section1:Text("new")
+    -- sets the section's text to something new
+
+    -- / Section1:Hide()
+    -- hides the section from eye view
+
+    -- / Section1:Show()
+    -- makes the section visible
+
+    -- / Section1:Remove()
+    -- destroys the section
+
+    __________________________
+
+    -- [Toggles contain rich text]
+
+    -- / Toggle1:Text("new")
+    -- sets the toggle's text to something new
+
+    -- / Toggle1:Hide()
+    -- hides the toggle from eye view
+
+    -- / Toggle1:Show()
+    -- makes the toggle visible
+
+    -- / Toggle1:Remove()
+    -- destroys the toggle
+
+    -- / Toggle1:Change()
+    -- changes the toggles state to the opposite
+
+    -- / Toggle1:Set(true)
+    -- sets the toggle to true even if it is true
+
+    -- / Toggle1:AddKeybind(Enum.KeyCode.P, false, function() end) -- false / true is used for just changing the toggles state
+    -- adds a keybind to the toggle
+
+    -- / Toggle1:SetFunction(function() end)
+    -- sets the toggles function
+
+    __________________________
+
+    -- [Keybinds contain rich text]
+
+    -- / Keybind1:Text("new")
+    -- sets the keybind's text to something new
+
+    -- / Keybind1:Hide()
+    -- hides the keybind from eye view
+
+    -- / Keybind1:Show()
+    -- makes the keybind visible
+
+    -- / Keybind1:Remove()
+    -- destroys the keybind
+
+    -- / Keybind1:SetKey(Enum.KeyCode.P)
+    -- sets the keybinds new key
+
+    -- / Keybind1:Fire()
+    -- fires the keybind function
+
+    -- / Keybind1:SetFunction(function() end)
+    -- sets the new keybind function
+
+    __________________________
+
+    -- [Textboxes contain rich text]
+
+    -- / Textbox1:Input("new")
+    -- sets the text box's input to something new
+
+    -- / Textbox1:Place("new")
+    -- sets the text box's placeholder to something new
+
+    -- / Textbox1:Fire()
+    -- fires the textbox function
+
+    -- / Textbox1:SetFunction(function() end)
+    -- sets the text boxes new function
+
+    -- / Textbox1:Text("new")
+    -- sets the textbox's text to something new
+
+    -- / Textbox1:Hide()
+    -- hides the textbox from eye view
+
+    -- / Textbox1:Show()
+    -- makes the textbox visible
+
+    -- / Textbox1:Remove()
+    -- destroys the textbox
+
+    __________________________
+
+    -- [Selectors contain rich text]
+
+    -- / Selector1:SetFunction(function() end)
+    -- sets the selector new function
+
+    -- / Selector1:Text("new")
+    -- sets the selector's text to something new
+
+    -- / Selector1:Hide()
+    -- hides the selector from eye view
+
+    -- / Selector1:Show()
+    -- makes the selector visible
+
+    -- / Selector1:Remove()
+    -- destroys the selector
+
+    __________________________
+
+    -- [Sliders contain rich text]
+
+    -- / Slider1:Value(1)
+    -- sets the slider new value
+
+    -- / Slider1:SetFunction(function() end)
+    -- sets the slider new function
+
+    -- / Slider1:Text("new")
+    -- sets the slider's text to something new
+
+    -- / Slider1:Hide()
+    -- hides the slider from eye view
+
+    -- / Slider1:Show()
+    -- makes the slider visible
+
+    -- / Slider1:Remove()
+    -- destroys the slider
+
+    ---------------------------------------------------------------------------------------------------------
+
+    MISC SEMI USELESS DOCS:
+
+    -- / library.rank = ""
+    -- returns the rank you choose (default = "private")
+
+    -- / library.fps
+    -- returns FPS you're getting in game
+
+    -- / library.version
+    -- returns the version of the library
+
+    -- / library.title = ""
+    -- returns the title of the library
+
+    -- / library:GetDay("word") -- word, short, month, year
+    -- returns the os day
+
+    -- / library:GetTime("24h") -- 24h, 12h, minute, half, second, full, ISO, zone
+    -- returns the os time
+
+    -- / library:GetMonth("word") -- word, short, digit
+    -- returns the os month
+
+    -- / library:GetWeek("year_S") -- year_S, day, year_M
+    -- returns the os week
+
+    -- / library:GetYear("digits") -- digits, full
+    -- returns the os year
+
+    -- / library:GetUsername()
+    -- returns the localplayers username
+
+    -- / library:GetUserId()
+    -- returns the localplayers userid
+
+    -- / library:GetPlaceId()
+    -- returns the games place id
+
+    -- / library:GetJobId()
+    -- returns the games job id
+
+    -- / library:CheckIfLoaded()
+    -- returns true if you're fully loaded
+
+    -- / library:Rejoin()
+    -- rejoins the same server as you was in
+
+    -- / library:Copy("stuff")
+    -- copies the inputed string
+
+    -- / library:UnlockFps(500) -- only works with synapse
+    -- sets the max fps to something you choose
+    
+    -- / library:PromptDiscord("invite")
+    -- invites you to a discord
+]]
